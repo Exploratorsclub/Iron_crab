792f09fb23edb67f8daa4b8ca12ac36aea0084f0
Robonuk <robert.onuk@gmail.com>
2025-12-18 21:26:25 +0100

    Fix build error: use solana_client::rpc_client::GetConfirmedSignaturesForAddress2Config

--- src/solana/sniper.rs
+++ src/solana/sniper.rs
@@ -4235,7 +4235,7 @@ impl SniperEngine {
             .rpc
             .get_signatures_for_address_with_config(
                 mint,
-                solana_client::rpc_config::GetConfirmedSignaturesForAddress2Config {
+                solana_client::rpc_client::GetConfirmedSignaturesForAddress2Config {
                     limit: Some(50),
                     ..Default::default()
                 },
